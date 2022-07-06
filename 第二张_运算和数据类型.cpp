d122b98fe8e1a8320bc0cdf879b5ccccc245a53e
Picking720 <45487142+Picking720@users.noreply.github.com>
2022-07-06 08:36:46 +0800

    Add files via upload

---
+++ 第二张_运算和数据类型.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main() {
+//	2-1四则运算;c语言的转义字符%
+//	int vx, vy;
+//
+//	puts("请输入两个整数:");
+//	printf("整数vx:");
+//	scanf("%d", &vx);
+//
+//	printf("整数2:");
+//	scanf("%d", &vy);
+//
+//	printf("%d+%d=%d\n", vx, vy, vx + vy);
+//	printf("%d-%d=%d\n", vx, vy, vx - vy);
+//	printf("%d*%d=%d\n", vx, vy, vx * vy);
+//	printf("%d/%d=%d\n", vx, vy, vx / vy);
+//	printf("%d %% %d=%d\n", vx, vy , vx % vy);
+
+
+
+//	代码清单2-2；获取整数最后一位数字no%10
+//	int no;
+//	printf("请输入一个整数：");
+//	scanf("%d", &no);
+//	printf("%d的最后一位数字是：%d", no, no % 10);
+
+//	练习2-1
+//	int n1, n2;
+//
+//	puts("请输入两个数字：");
+//	printf("整数A:");
+//	scanf("%d", &n1);
+//
+//	printf("整数B:");
+//	scanf("%d", &n2);
+//
+//
+//	printf("A是B的%d%%", (n1 * 100) / n2);
+
+//	代码清单2-3
+//	int na, nb;
+//	puts("请输入两个整数：");
+//	printf("整数A：");
+//	scanf("%d", &na);
+//	printf("整数B：");
+//	scanf("%d", &nb);
+//
+//	printf("A除以B得到%d余数为%d", na / nb, na % nb);
+
+//	练习2-2
+//	int n1, n2;
+//	puts("请输入两个整数：");
+//	printf("整数A:");
+//	scanf("%d", &n1);
+//	printf("整数B:");
+//	scanf("%d", &n2);
+//	printf("它们的和是%d,它们的积是%d", n1 + n2, n1 * n2);
+
+//	代码清单2-4；取反操作
+//	int num;
+//	printf("请输入一个整数：");
+//	scanf("%d", &num);
+//	printf("符号取反之后的值是%d\n", -num);
+
+//page----------35
+
+
+
+	return 0;
+}
